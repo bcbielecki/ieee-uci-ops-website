7eac8a2501a6f1fe58533f450298e47bf3b97432
bcbielecki <bcbielecki@gmail.com>
2023-06-30 00:55:23 -0700

    init

---
+++ src/js/ops_header.js
@@ -0,0 +1,65 @@
+document.write(`
+<header class="navbar navbar-expand-lg bg-primary sticky-top"  data-bs-theme="dark">
+    <nav class="container-xl">
+            <a class="navbar-brand" href="#">
+                <img src="./assets/ops_logo_nobg_100ppi.png" width="50px" alt="OPS Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Syllabus</a>
+                </li>
+                <li class="nav-item dropdown ">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Projects
+                    </a>
+                    <ul class="dropdown-menu" data-bs-theme="light" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Project 1: LED Circuit</a></li>
+                    <li><a class="dropdown-item" href="#">Project 2: 555 Piano</a></li>
+                    <li><a class="dropdown-item" href="#">Project 3: C++ Programming Exercises</a></li>
+                    <li><a class="dropdown-item" href="#">Project 4: RGB Dimmer</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Project 5: iPoduino v2</a></li>
+                    <li><a class="dropdown-item" href="#">Project 6: Weather Station</a></li>
+                    <li><a class="dropdown-item" href="#">Project 7: 7-Segment Stopwatch</a></li>
+                    <li><a class="dropdown-item" href="#">Project 8: 555 Blinker PCB Design</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Capstone Project: RC Rover</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown ">
+                    <a class="nav-link dropdown-toggle disabled" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Lectures
+                    </a>
+                    <ul class="dropdown-menu" data-bs-theme="light" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Project 1: LED Circuit</a></li>
+                    <li><a class="dropdown-item" href="#">Project 2: 555 Piano</a></li>
+                    <li><a class="dropdown-item" href="#">Capstone Project: RC Rover</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle disabled" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Workshops
+                </a>
+                <ul class="dropdown-menu" data-bs-theme="light" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Project 1: LED Circuit</a></li>
+                <li><a class="dropdown-item" href="#">Project 2: 555 Piano</a></li>
+                <li><a class="dropdown-item" href="#">Capstone Project: RC Rover</a></li>
+                </ul>
+            </li>
+                </ul>
+                <form class="d-flex" data-bs-theme="light">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-secondary" type="submit">Search</button>
+                </form>
+            </div>
+    </nav>
+</header>
+
+`)
