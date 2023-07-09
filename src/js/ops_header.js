a6711b3e65837890d4750b332d7b585f5b529417
bcbielecki <bcbielecki@gmail.com>
2023-07-08 19:04:01 -0700

    reduced landing text and added syllabus page

--- src/js/ops_header.js
+++ src/js/ops_header.js
@@ -13,7 +13,7 @@ document.write(`
                         <a class="nav-link" aria-current="page" href="./index.html">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Syllabus</a>
+                        <a class="nav-link" href="./syllabus.html">Syllabus</a>
                     </li>
                     <li class="nav-item dropdown ">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
